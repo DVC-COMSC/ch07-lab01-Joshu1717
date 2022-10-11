61ae732063f2781e0c8b0a89658c58a07ea71cec
Joshua Thaxton <joshuathaxton540@gmail.com>
2022-10-10 21:01:46 -0700

    Lab1

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 # ******************************
 # Make your Code
 # ******************************
+def main():
+    total = 0
+    
+
+    num = list(map(int, input("Enter 5 numbers:").split()))
+    print (min(num))
+    print (max(num))
+
+    for ele in range(0, len(num)):
+        total = total + num[ele]
+    
+    total = total - min(num) - max(num)
+
+    print("Sum of all numbers minus the smallest and largest numbers: ", total)
+
+main()
